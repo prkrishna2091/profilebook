6db9d47eff9752ab7522751fcdf08f781e2b5be1
prkrishna2091 <prkrishna2091@gmail.com>
2023-07-09 20:22:26 +0530

    project

---
+++ src/main/java/com/rk/profilebook/controller/ProfileBookController.java
@@ -0,0 +1,17 @@
+package com.rk.profilebook.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pbook")
+public class ProfileBookController {
+
+    @GetMapping("/welcome")
+    public String welcomeMessage(){
+        return "Welcome to Profile Book...!";
+    }
+
+}
---
+++ src/test/java/com/rk/profilebook/ProfilebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.profilebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
